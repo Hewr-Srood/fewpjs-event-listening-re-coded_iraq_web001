7313677fecade0618fb4873036db09df242349b7
Hewr Srood <hewr_s@outlook.com>
2020-05-28 18:11:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+
+
 function addingEventListener() {
   const input = document.getElementById('input');
   input.addEventListener('click', function(event) {
+<<<<<<< HEAD
     alert('I was clicked!');
   });
 }
+=======
+    
+  });
+}
+>>>>>>> 83e538e6d813ff9802de253e6f7f7713b5773447
